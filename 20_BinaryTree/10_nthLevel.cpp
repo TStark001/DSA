76b012b62ec3ce681f09c3569f5e38f57b468eb8
TStark001 <hc2005023@gmail.com>
2026-01-31 18:34:04 +0530

    Updating code

--- 20_BinaryTree/10_nthLevel.cpp
+++ 20_BinaryTree/10_nthLevel.cpp
@@ -13,6 +13,10 @@ public:
         this->right = NULL;
     }
 };
+int level(Node* root){ // Levels of Tree
+    if(root == NULL) return 0;
+    return 1 + max(level(root->left),level(root->right));
+}
 void preOrder(Node* root){
     if(root == NULL) return;
     cout<<root->val<<" "; 
@@ -25,7 +29,29 @@ void nthLevel(Node* root,int curr,int level){
     nthLevel(root->left,curr+1,level); 
     nthLevel(root->right,curr+1,level); 
 }
-
+/*
+    this will print in reverse order
+    output:-
+            1 
+            3 2 
+            7 6 5 4 
+*/ 
+void nthLevelRev(Node* root,int curr,int level){
+    if(root == NULL) return;
+    if(curr==level) {
+        cout<<root->val<<" ";
+        return;
+    } 
+    nthLevelRev(root->right,curr+1,level); 
+    nthLevelRev(root->left,curr+1,level); 
+}
+void levelOrder(Node* root){
+    int n = level(root);
+    for(int i = 1 ; i<=n ; i++){
+        nthLevel(root,1,i);
+        cout<<endl;
+    }
+}
 
 int main(){
     Node* a = new Node(1);
@@ -43,5 +69,5 @@ int main(){
     b->right = e;
     c->left = f;
     c->right = g;
-    nthLevel(a,1,3);
+    levelOrder(a);
 }
